cca8a3e99cdac1a8ff3e9f33e42853923d2b7b7b
Seenu24492 <seenu24492@gmail.com>
2025-01-17 13:58:26 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+ #!/bin/sh
+
+# Read the content of hello.txt
+content=$(cat hello.txt)
+
+# Check if the content is "Hello, world!"
+if [ "$content" = "Hello, world!" ]; then
+    echo "Test passed"
+    exit 0
+else
+    echo "Test failed"
+    exit 1
+fi
